605fb6e5532923972d366a5064026ee9fcbdc281
Aman <pandeyaman0701@gmail.com>
2021-11-28 00:46:59 +0530

    added strings for app

---
+++ src/assets/strings.js
@@ -0,0 +1,18 @@
+export const ShoppingCartStrings = {
+    ReloadText: "Reload",
+    OrderSummaryText: "< Order Summary",
+    ItemRemovedText: " Removed",
+    BillTotalTitleText: "Total",
+    ItemsTitleText: "Items",
+    TotalDiscountText: "Discount :",
+    TotalTypeDiscountText: "Type Discount : ",
+    TotalOrderText: "Order Total: ",
+    QuantityTitleText: "Qty",
+    PriceTitleText: "Price",
+    PriceUnitSymbol: "$",
+    DecreaseQuantityButtonText: "-",
+    IncreaseQuantityButtonText: "+",
+    DeleteItemButtonText: "X",
+    ItemImageAltText: "Item",
+    FictionTypeItem: "fiction"
+}
