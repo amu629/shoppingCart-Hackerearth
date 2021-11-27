c0a721bced465362bc89dc59e3db405fac437b8b
Aman <pandeyaman0701@gmail.com>
2021-11-28 00:39:20 +0530

    adding index js and css files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ShoppingCart from './ShoppingCart';
+
+
+ReactDOM.render(
+    <ShoppingCart />,
+  document.getElementById('root')
+);
+
